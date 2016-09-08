07273920631a389678458606fe41f40c2af79c52
Mohamed Saqer <mohamed.saqer@outlook.com>
2016-09-08 23:48:41 +0200

    9.9.2016

--- SplitViewTemplate/SplitViewTemplate/App.xaml.cs
+++ SplitViewTemplate/SplitViewTemplate/App.xaml.cs
@@ -7,6 +7,7 @@ using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -76,6 +77,8 @@ namespace SplitViewTemplate
                 // configuring the new page by passing required information as a navigation
                 // parameter
                 rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                //     ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.FullScreen;
+                ApplicationView.PreferredLaunchViewSize = ApplicationView.PreferredLaunchViewSize;
             }
             // Ensure the current window is active
             Window.Current.Activate();
